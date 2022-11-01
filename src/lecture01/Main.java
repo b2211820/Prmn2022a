8394ec337b975c52034527a654900cc3d5d1683c
b2211820 <b2211820@photon.chitose.ac.jp>
2022-11-01 11:49:53 +0900

    kadai1-2

--- src/lecture01/Main.java
+++ src/lecture01/Main.java
@@ -2,6 +2,8 @@ package lecture01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("B2211820 Haruto Nishizawa");
+        //System.out.println("B2211820 Haruto Nishizawa");
+      int studentNumber = 2211820;
+      System.out.println("B" + studentNumber + " Haruto Nishizawa");
     }
 }
