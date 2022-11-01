3dba45f28f3156affe7a82c3d8adfc83ca6064a6
b2211820 <b2211820@photon.chitose.ac.jp>
2022-11-01 12:04:02 +0900

    kadai1-3

--- src/lecture01/Main.java
+++ src/lecture01/Main.java
@@ -1,9 +1,23 @@
 package lecture01;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //System.out.println("B2211820 Haruto Nishizawa");
-      int studentNumber = 2211820;
-      System.out.println("B" + studentNumber + " Haruto Nishizawa");
+        //int studentNumber = 2211820;
+        // System.out.println("B" + studentNumber + " Haruto Nishizawa");
+
+      System.out.println("input number");
+      Scanner input = new Scanner(System.in);
+      int age = input.nextInt();
+
+      if(age < 20){
+          System.out.println("I am "+ age +" years old so I cannot drink liquor");
+      }
+      else{
+          System.out.println("I am "+ age +" years old so I can drink liquor");
+      }
+
     }
 }
